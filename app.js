ae133b3da352e317d599e3e961984f85526da846
gamze <worlfee@gmail.com>
2019-05-07 13:50:49 +0300

    a little bit done

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const port = 3000;
 
 app.get('/user', (req, res) => {
   res.send({
-    firstName:'Uğur',
-    lastName:'KARAKAYA',
+    firstName:'Gamze',
+    lastName:'YAPICI',
     age:36,
-    mail:'ugurkarakaya1@gmail.com'
-  }).status(200).end();
+    mail:'gaamzee.yaapiicii@gmail.com'
+  });
 });
 
-app.get('/', (req, res) => {
-  res.send('SELAM').status(200).end();
-});
+app.get('/', (req, res) => res.sendFile(path.resolve(`${__dirname}/index.html`)));
+
+app.post('/', function (req, res) {
+  res.send('Got a POST request')
+})
 
 
 
