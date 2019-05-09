35ba660a9a1f264ad29786eecfde2169ebf999d1
gamze <worlfee@gmail.com>
2019-05-09 17:06:02 +0300

    done

--- app/user/user.component.js
+++ app/user/user.component.js
@@ -1,17 +1,23 @@
 (function () {
     var controller = function ($scope) {
         $scope.user = 'Home Page';
-        $scope.init=function(){}        
+        // $scope.init=function(){}        
+
+        $scope.collapsed = true;
+        $scope.toggleCollapsed = function () {
+            $scope.collapsed = !$scope.collapsed;
+        };
+
     }
 
     angular.module('app')
         .component('user', {
             templateUrl: 'app/user/user.component.html',
             controller: controller
-        }).config(function($stateProvider){
+        }).config(function ($stateProvider) {
             $stateProvider.state('user', {
                 url: '/',
-                template: '<user></user>'                
+                template: '<user></user>'
             })
         });
 })();
